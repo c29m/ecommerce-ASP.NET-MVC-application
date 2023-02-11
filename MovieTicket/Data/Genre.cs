1118fc202fe051055b2db1da0a3e41d9163d0914
c29m <c29.hunter@gmail.com>
2023-02-11 21:07:42 +0200

    Movie and Cinema models implementation

---
+++ MovieTicket/Data/Genre.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieTicket.Data
+{
+    public enum Genre
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Horror,
+        Documentary,
+        Crime,
+        Detective
+    }
+}
---
+++ MovieTicket/Models/Cinema.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicket.Models
+{
+    public class Cinema
+    {
+        [Key]
+
+
+        public int CinemaId { get; set; }
+
+
+        public string Logo { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
