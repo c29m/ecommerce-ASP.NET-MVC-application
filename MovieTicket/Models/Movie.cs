fc7045cc38f9f60695b9f40f0569d93e2c3883f0
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-11 21:07:42 +0200

    Movie and Cinema models implementation

---
+++ MovieTicket/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MovieTicket.Data;
+
+namespace MovieTicket.Models
+{
+    public class Movie
+    {
+        [Key]
+
+        public int MovieId { get; set; }
+
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public Genre Genre { get; set; }
+
+
+    }
+}
