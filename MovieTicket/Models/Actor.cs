4e75ffc08a9422487a6b426b44f17fb74502a664
c29m <c29.hunter@gmail.com>
2023-02-11 21:00:39 +0200

    Actor model implimentation

---
+++ MovieTicket/Models/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicket.Models
+{
+    public class Actor
+    {
+        [Key]
+
+        public int ActorId { get; set; }
+
+
+        public string ProfilePictureURL { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
