a35b3314f317723b8168d81d31df34a42b47abfe
c29m <c29.hunter@gmail.com>
2023-02-11 21:02:58 +0200

    Director model implementation

---
+++ MovieTicket/Models/Director.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicket.Models
+{
+    public class Director
+    {
+        [Key]
+
+        public int DirectorId { get; set; }
+
+
+        public string ProfilePictureURL { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
