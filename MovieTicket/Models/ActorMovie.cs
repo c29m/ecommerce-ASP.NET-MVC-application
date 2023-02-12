0e6d01bf2d021e46dc0b5c498c006d568ddaf4a4
c29m <c29.hunter@gmail.com>
2023-02-12 20:09:03 +0200

    Defining relationships

---
+++ MovieTicket/Models/ActorMovie.cs
@@ -0,0 +1,15 @@
+﻿namespace MovieTicket.Models
+{
+    public class ActorMovie
+    {
+        public int MovieId { get; set; }
+
+        public Movie Movie { get; set; }
+
+
+        public int ActorId { get; set; }
+
+        public Actor Actor { get; set; }
+
+    }
+}
--- MovieTicket/Models/Cinema.cs
+++ MovieTicket/Models/Cinema.cs
@@ -15,5 +15,10 @@ namespace MovieTicket.Models
         public string Name { get; set; }
 
         public string Description { get; set; }
+
+
+        /*-----Relationships-----*/
+
+        public List<Movie> Movies { get; set; }
     }
 }
