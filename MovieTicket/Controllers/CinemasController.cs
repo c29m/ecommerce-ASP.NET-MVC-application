1badddeca67a72505e687fd770cd12090983929f
c29m <c29.hunter@gmail.com>
2023-02-14 17:41:11 +0200

    Comtrollers implementation

---
+++ MovieTicket/Controllers/CinemasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieTicket.Data;
+
+namespace MovieTicket.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+
+        public CinemasController(AppDbContext appDbContext)
+        {
+
+            _appDbContext = appDbContext;
+
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _appDbContext.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ MovieTicket/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using MovieTicket.Data;
+
+namespace MovieTicket.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+
+        public MoviesController(AppDbContext appDbContext)
+        {
+
+            _appDbContext = appDbContext;
+
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = await _appDbContext.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
