e1699bc52ef9a532036afc8d66379ff6aa1dec62
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-14 17:41:11 +0200

    Comtrollers implementation

---
+++ MovieTicket/Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieTicket.Data;
+
+namespace MovieTicket.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+
+        public ActorsController(AppDbContext appDbContext)
+        {
+
+            _appDbContext = appDbContext;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allActors = await _appDbContext.Actors.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ MovieTicket/Controllers/DirectorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieTicket.Data;
+
+namespace MovieTicket.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+
+        public DirectorsController(AppDbContext appDbContext)
+        {
+
+            _appDbContext = appDbContext;
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allSDirectors = await _appDbContext.Directors.ToListAsync();
+            return View();
+        }
+    }
+}
