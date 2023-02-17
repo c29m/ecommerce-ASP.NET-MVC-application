fae7c1607e47c9dc3d213081578400cfdd45658f
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-17 22:45:08 +0200

    updated actors service

--- MovieTicket/Models/Actor.cs
+++ MovieTicket/Models/Actor.cs
@@ -1,26 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MovieTicket.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieTicket.Models
 {
-    public class Actor
+    public class Actor : IEntityBase
     {
         [Key]
-
-        public int ActorId { get; set; }
-
+        public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
-
-        /*-----Relationships-----*/
-
-        public List<ActorMovie> ActorsMovies { get; set; }
+   
+        public List<ActorMovie>? ActorsMovies { get; set; }
     }
 }
