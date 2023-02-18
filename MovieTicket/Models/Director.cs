c5662b5cc171513f982a9fdc1d12e0523be3e6d0
c29m <c29.hunter@gmail.com>
2023-02-18 17:36:53 +0200

    added director validation

--- MovieTicket/Models/Director.cs
+++ MovieTicket/Models/Director.cs
@@ -10,12 +10,15 @@ namespace MovieTicket.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
 
