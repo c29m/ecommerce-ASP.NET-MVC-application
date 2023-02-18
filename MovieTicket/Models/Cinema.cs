1a39d2c88f1746628a14f4dc01a7844e35ad9673
c29m <c29.hunter@gmail.com>
2023-02-18 21:04:16 +0200

    added cinema validation

--- MovieTicket/Models/Cinema.cs
+++ MovieTicket/Models/Cinema.cs
@@ -1,26 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MovieTicket.Data.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieTicket.Models
 {
-    public class Cinema
+    public class Cinema : IEntityBase
     {
         [Key]
 
 
-        public int CinemaId { get; set; }
+        public int Id { get; set; }
 
         [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage = "Cinema Logo is required")]
         public string Logo { get; set; }
 
         [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Cinema Name is required")]
         public string Name { get; set; }
 
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
 
         /*-----Relationships-----*/
 
-        public List<Movie> Movies { get; set; }
+        public List<Movie>? Movies { get; set; }
     }
 }
