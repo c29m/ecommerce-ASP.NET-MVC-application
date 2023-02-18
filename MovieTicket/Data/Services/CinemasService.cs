68618fae86854b734b0db62e442e224579d5b7a2
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-18 17:23:02 +0200

    cinema service configured

---
+++ MovieTicket/Data/Services/CinemasService.cs
@@ -0,0 +1,10 @@
+﻿using MovieTicket.Models;
+using MovieTicket.Data.Base;
+
+namespace MovieTicket.Data.Services
+{
+    public class CinemasService : EntityBaseRepository<Cinema>, ICinemasService
+    {
+        public CinemasService(AppDbContext context) : base(context) { }
+    }
+}
