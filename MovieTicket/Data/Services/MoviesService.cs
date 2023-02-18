98b9715997d14d883f0108f3fd60388b5e7392da
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-18 22:04:37 +0200

    added movie page details

--- MovieTicket/Data/Services/MoviesService.cs
+++ MovieTicket/Data/Services/MoviesService.cs
@@ -1,13 +1,29 @@
-﻿using MovieTicket.Data.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTicket.Data.Base;
 using MovieTicket.Models;
 
 namespace MovieTicket.Data.Services
 {
     public class MoviesService : EntityBaseRepository<Movie>, IMoviesService
     {
+        private readonly AppDbContext _context;
+
         public MoviesService(AppDbContext context): base(context)
         {
-
+            _context = context;
         }
+
+
+        public async Task<Movie> GetMovieByIdAsync(int id)
+        {
+            var movieDetails = await _context.Movies
+                .Include(c => c.Cinema)
+                .Include(p => p.Director)
+                .Include(am => am.MovieActors).ThenInclude(a => a.Actor)
+                .FirstOrDefaultAsync(n => n.Id == id);
+
+            return movieDetails;
+        }
+
     }
 }
