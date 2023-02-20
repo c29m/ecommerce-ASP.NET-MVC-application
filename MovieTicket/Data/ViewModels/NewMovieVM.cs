ca9f62a21de36db23a7a9896c303c17d13eafd1e
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-20 21:50:37 +0200

    add new movies

---
+++ MovieTicket/Data/ViewModels/NewMovieVM.cs
@@ -0,0 +1,50 @@
+﻿using MovieTicket.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicket.Models
+{
+    public class NewMovieVM
+    {
+        [Display(Name = "Movie name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Movie description")]
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        [Display(Name = "Price in $")]
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [Display(Name = "Movie poster URL")]
+        [Required(ErrorMessage = "Movie poster URL is required")]
+        public string ImageURL { get; set; }
+
+        [Display(Name = "Movie start date")]
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Movie end date")]
+        [Required(ErrorMessage = "End date is required")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Select a genre")]
+        [Required(ErrorMessage = "Movie genre is required")]
+        public Genre Genre { get; set; }
+
+        //Relationships
+        [Display(Name = "Select actor(s)")]
+        [Required(ErrorMessage = "Movie actor(s) is required")]
+        public List<int> ActorIds { get; set; }
+
+        [Display(Name = "Select a cinema")]
+        [Required(ErrorMessage = "Movie cinema is required")]
+        public int CinemaId { get; set; }
+
+        [Display(Name = "Select a director")]
+        [Required(ErrorMessage = "Movie director is required")]
+        public int DirectorId { get; set; }
+    }
+}
+
