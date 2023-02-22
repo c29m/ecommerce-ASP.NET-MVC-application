e0900b4e2896b5db9febd2639e78a1f78b2e98cd
c29m <c29.hunter@gmail.com>
2023-02-22 23:44:14 +0200

    added orders controller

---
+++ MovieTicket/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTicket.Data.Cart;
+using MovieTicket.Data.Services;
+using MovieTicket.Data.ViewModels;
+
+namespace MovieTicket.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+
+            return View(response);
+        }
+    }
+}
