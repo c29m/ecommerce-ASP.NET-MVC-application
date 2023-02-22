d60e70d062867715ee38e5357ee1f2f075be3043
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-22 23:44:14 +0200

    added orders controller

---
+++ MovieTicket/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using MovieTicket.Data.Cart;
+
+namespace MovieTicket.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
