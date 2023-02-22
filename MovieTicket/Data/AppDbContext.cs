01d2207b3beb679e749bba50b12c918ee243b10c
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-22 22:45:31 +0200

    added shopping cart

--- MovieTicket/Data/AppDbContext.cs
+++ MovieTicket/Data/AppDbContext.cs
@@ -42,5 +42,7 @@ namespace MovieTicket.Data
        public DbSet<Order> Orders { get; set; }
 
        public DbSet<OrderItem> OrderItems { get; set; }
+
+       public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
---
+++ MovieTicket/Models/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicket.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+        public Movie Movie { get; set; }
+        
+        public string ShoppingCartId {get; set; } 
+
+    }
+}
