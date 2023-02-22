4fe009a42ed760e8ce92ae2c37efd4415f0c38da
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-22 23:42:46 +0200

    added removeItemFromCart method

--- MovieTicket/Data/Cart/ShoppingCart.cs
+++ MovieTicket/Data/Cart/ShoppingCart.cs
@@ -36,6 +36,24 @@ namespace MovieTicket.Data.Cart
             _context.SaveChanges();
          }
 
+        public void RemoveItemFromCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if(shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                }
+                else
+                {
+                    _context.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _context.SaveChanges();
+        }
 
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
