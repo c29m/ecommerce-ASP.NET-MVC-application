33247db91d8c6812ec583602918c87d3a2efa424
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-24 00:01:41 +0200

    added and configured ordersService

--- MovieTicket/Program.cs
+++ MovieTicket/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddScoped<IActorsService, ActorsService>();
 builder.Services.AddScoped<IDirectorsService, DirectorsService>();
 builder.Services.AddScoped<ICinemasService, CinemasService>();
 builder.Services.AddScoped<IMoviesService, MoviesService>();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
