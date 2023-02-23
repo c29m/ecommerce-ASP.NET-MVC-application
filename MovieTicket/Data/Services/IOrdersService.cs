8f85682dce9c9a0c1b1863f7b689d677ce7826dd
c29m <c29.hunter@gmail.com>
2023-02-24 00:01:41 +0200

    added and configured ordersService

---
+++ MovieTicket/Data/Services/IOrdersService.cs
@@ -0,0 +1,12 @@
+﻿using MovieTicket.Models;
+
+namespace MovieTicket.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrdedrAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+
+        Task<List<Order>> GetAllOrdersByUserIdAsync(string userId);
+        
+    }
+}
---
+++ MovieTicket/Data/Services/OrdersService.cs
@@ -0,0 +1,50 @@
+﻿using AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using MovieTicket.Models;
+
+namespace MovieTicket.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly AppDbContext _context;
+
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<Order>> GetAllOrdersByUserIdAsync(string userId)
+        {
+            var orders = await _context.Orders.Include(n => n.OrderItems).ThenInclude(n => n.Movie).Where( n => n.UserId == userId).ToListAsync();
+
+            return orders;
+        }
+
+        public async Task StoreOrdedrAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress)
+        {
+            var order = new Order()
+            {
+                UserId = userId,
+                Email = userEmailAddress
+            };
+
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+
+            foreach(var item in items)
+            {
+                var orderItem = new OrderItem()
+                {
+                    Amount = item.Amount,
+                    MovieId = item.Movie.Id,
+                    OrderId = order.Id,
+                    Price = item.Movie.Price
+
+                };
+                await _context.AddAsync(orderItem);
+               }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
