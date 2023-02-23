893147b0ad54a2b1f6372ce39a5d6946f2384ba6
c29m <c29.hunter@gmail.com>
2023-02-23 20:42:55 +0200

    added "add item to shopping cart"

--- MovieTicket/Controllers/OrdersController.cs
+++ MovieTicket/Controllers/OrdersController.cs
@@ -30,5 +30,19 @@ namespace MovieTicket.Controllers
 
             return View(response);
         }
+
+        public async Task<RedirectToActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _moviesService.GetByIdAsync(id);
+
+            if(item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+
     }
 }
