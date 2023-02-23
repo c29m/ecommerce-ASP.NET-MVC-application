86fdae0067b40082a3ba6fd0c9007c011f15fb11
c29m <c29.hunter@gmail.com>
2023-02-23 19:53:38 +0200

    configuring shopping cart session

--- MovieTicket/Data/Cart/ShoppingCart.cs
+++ MovieTicket/Data/Cart/ShoppingCart.cs
@@ -7,12 +7,24 @@ namespace MovieTicket.Data.Cart
     {
         public AppDbContext _context { get; set; }  
 
-        public string ShoppingCartId { get; set; }  
+        public string ShoppingCartId { get; set; } 
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
 
         public ShoppingCart(AppDbContext context)
         {
              _context = context;
+        }
+        public static ShoppingCart GetShoppingCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            var context = services.GetService<AppDbContext>();
+
+            string cartId = session.GetString("CartId")??Guid.NewGuid().ToString();
+
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+
         }
 
         public void AddItemToCart(Movie movie)
