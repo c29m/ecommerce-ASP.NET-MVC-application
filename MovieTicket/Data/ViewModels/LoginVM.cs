0e47bd146409d4d8250c4121a6c1b9670d7e1e91
c29m <c29.hunter@gmail.com>
2023-02-24 23:57:50 +0200

    added login view

---
+++ MovieTicket/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTicket.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "Email addrss")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }    
+
+    }
+}
