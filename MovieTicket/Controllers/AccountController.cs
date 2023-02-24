1af3a625b59782f420d5cca1268c479452d21e1d
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-02-24 22:35:56 +0200

    added accountcontroller

---
+++ MovieTicket/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MovieTicket.Data;
+using MovieTicket.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieTicket.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager; 
+            _context = context; 
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
